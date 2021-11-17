f35f7618f0b36531dc010cf74150b9496c28738a
sbalaji1192 <sbalaji1192@gmail.com>
2021-11-18 00:31:03 +0530

    wip

--- web/src/components/UI/atoms/icon.tsx
+++ web/src/components/UI/atoms/icon.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaAngleDown, FaAngleUp } from "react-icons/fa";
 import { FcAndroidOs } from "react-icons/fc";
 import { FaApple } from "react-icons/fa";
 import { DiSafari } from "react-icons/di";
 
 type IconProps = {
   name: string;
+  onClick: () => void;
 };
 
-export default function Icon({ name }: IconProps) {
+export default function Icon({ name, onClick }: IconProps) {
   let icon;
 
   switch (name) {
@@ -24,8 +25,18 @@ export default function Icon({ name }: IconProps) {
     case "safari":
       icon = <DiSafari />
       break;
+    case "arrow-up":
+      icon = <FaAngleUp />
+      break;
+    case "arrow-down":
+      icon = <FaAngleDown />
+      break;
     default:
       icon = null;
   }
-  return <span>{icon}</span>;
+  return (
+    <span onClick={onClick}>
+      {icon}
+    </span>
+  );
 }
--- web/src/components/UI/organisms/session-debug-log-entry.tsx
+++ web/src/components/UI/organisms/session-debug-log-entry.tsx
@@ -5,8 +5,50 @@ import ArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import Moment from "react-moment";
 import CodeViewer from "../../../../../widgets/code-viewer/code-viewer";
 import styled from "styled-components";
+import { useState } from "react";
+import ParallelLayout, { Column } from "../layouts/parallel-layout";
+import Icon from "../atoms/icon";
 
 const Container = styled.div``;
+
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  margin: 2px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Content = styled.div``;
+
+const Time = styled.div`
+  font-size: 11px;
+  min-width: 150px;
+  font-style: italic;
+  color: grey;
+`;
+
+const Message = styled.div`
+  font-size: 11px;
+  margin-left: 15px;
+`;
+
+const Expand = styled.div``;
+
+const CodeViewerWrapper = styled.div`
+  width: 90%;
+  padding: 10px;
+  padding-right: 20px;
+  padding-left: 20px;
+  border: 1px solid #ced8e1;
+  background: #f9f9f9;
+  margin: auto;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  max-height: 200px;
+  overflow: scroll;
+`;
 
 type PropsType = {
   entry: any;
@@ -14,41 +56,46 @@ type PropsType = {
 
 export default function SessionDebugLogEntry(props: PropsType) {
   const { entry } = props;
+  const [isExpanded, setIsExpanded] = useState(false);
   return (
     <Container>
-      <div className="session-debug-log__entry-row">
-        <div className="session-debug-log__title_wrapper">
-          <div className="session-debug-log__time">
-            <Moment format="DD-MM-YYYY HH:mm:ss">
-              {this.props.log.timestamp}
-            </Moment>
-          </div>
-          <div className="session-debug-log__message">
-            {this.props.log.message}
-          </div>
-        </div>
-        <div className="session-test-title__right-container">
-          {this.props.log.args && (
-            <div className="expand-icon">
-              {this.state.expanded ? (
-                <ArrowUpIcon onClick={() => this.toggleExpand(false)} />
+      <Header>
+        <ParallelLayout>
+          <Column grid={11}>
+            <>
+              <Time>
+                <Moment format="DD-MM-YYYY HH:mm:ss">
+                  {entry.timestamp}
+                </Moment>
+              </Time>
+              <Message>
+                {entry.message}
+              </Message>
+            </>
+          </Column>
+          <Column grid={11}>
+            {entry.args && (
+            <Expand>
+              {isExpanded ? (
+                <Icon name="arrow-up" onClick={() => setIsExpanded(false)} />
               ) : (
-                <ArrowDownIcon onClick={() => this.toggleExpand(true)} />
+                <Icon name="arrow-down" onClick={() => setIsExpanded(false)} />
               )}
-            </div>
+            </Expand>
           )}
-        </div>
-      </div>
-      {this.props.log.args && this.state.expanded && (
-        <div className="session-debug-log__entry-row">
-          <div className="session-debug-log__args-wrapper">
+          </Column>
+        </ParallelLayout>
+      </Header>
+      <Content>
+        {entry.args && isExpanded && (
+          <CodeViewerWrapper>
             <CodeViewer
-              code={JSON.stringify(this.props.log.args, null, 2)}
+              code={JSON.stringify(entry.args, null, 2)}
               language={"json"}
             />
-          </div>
-        </div>
-      )}
+          </CodeViewerWrapper>
+        )}          
+      </Content>
     </Container>
   );
 }
