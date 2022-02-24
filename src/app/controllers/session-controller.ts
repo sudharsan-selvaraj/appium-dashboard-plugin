2c7a860653cc39a51f150983187095b21de1b6e2
sudharsan-selvaraj <sudharsanselvaraj.c@gmail.com>
2022-02-24 22:21:21 +0530

    feat:ability to view the live video in full screen

--- src/app/controllers/session-controller.ts
+++ src/app/controllers/session-controller.ts
@@ -75,16 +75,42 @@ export class SessionController extends BaseController {
 
   public async getVideoForSession(request: Request, response: Response, next: NextFunction) {
     let sessionId: string = request.params.sessionId;
+    const range = request.headers.range;
     let session = await Session.findOne({
       where: {
         session_id: sessionId,
       },
     });
-    if (session && session.video_path) {
-      return response.status(200).sendFile(session.video_path);
-    }
+    const videoPath = session?.video_path;
 
-    this.sendFailureResponse(response, "Video not available");
+    if (session && videoPath && range) {
+      const videoSize = fs.statSync(videoPath).size;
+      // Parse Range
+      // Example: "bytes=32324-"
+      const CHUNK_SIZE = 10 ** 6; // 1MB
+      const start = Number(range.replace(/\D/g, ""));
+      const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+      // Create headers
+      const contentLength = end - start + 1;
+      const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+      };
+
+      // HTTP Status 206 for Partial Content
+      response.writeHead(206, headers);
+
+      // create video read stream for this particular chunk
+      const videoStream = fs.createReadStream(videoPath, { start, end });
+
+      // Stream the video chunk to the client
+      videoStream.pipe(response);
+    } else {
+      this.sendFailureResponse(response, "Video not available");
+    }
   }
 
   public async getTextLogs(request: Request, response: Response, next: NextFunction) {
